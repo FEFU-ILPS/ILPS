7c2e5e6e2d39685f6ce83edcc1b2b309e43a99f9
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-14 16:42:43 +1000

    Мелкие фиксы

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-# ilps-auth-service database
-CREATE USER service_auth WITH PASSWORD "password123";
+-- ilps-auth-service database creation
+CREATE USER service_auth WITH PASSWORD 'password123';
 CREATE DATABASE auth;
 ALTER DATABASE auth OWNER TO service_auth;
