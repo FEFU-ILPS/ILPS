68e1f59b857380f0508e02e07eeef782071f62a7
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-03-24 22:08:25 +1000

    Создание пользователя и БД для сервиса текстов

--- init.sql
+++ init.sql
@@ -1,4 +1,9 @@
--- ilps-auth-service database creation
+-- ilps-service-auth database creation
 CREATE USER service_auth WITH PASSWORD 'password123';
 CREATE DATABASE auth;
 ALTER DATABASE auth OWNER TO service_auth;
+
+-- ilps-service-texts database creation
+CREATE USER service_texts WITH PASSWORD 'password123';
+CREATE DATABASE texts;
+ALTER DATABASE texts OWNER TO service_texts;
